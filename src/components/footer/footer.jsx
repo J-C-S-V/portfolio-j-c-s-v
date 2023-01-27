9c441e3a481500c1ba2d95aacf8dee157194aba8
Juan Sanchez <juansan1993@hotmail.com>
2023-01-26 23:08:49 -0500

    Working on the footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'footer.css';
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="contact-text"></div>
+      <div className="images"></div>
+    </footer>
+  );
+}

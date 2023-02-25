392072f4dde7b1f9158eeb6418f392962702107e
Juan Sanchez <juansan1993@hotmail.com>
2023-02-25 18:46:44 -0500

    Update class names to BEM in navbar section

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import './navbar.css';
 export function Navbar() {
   return (
-    <div className="navbar" id="nav-bar-id">
-      <div className="one-half">
+    <div className="navbar">
+      <div className="navbar__photo">
         <img
-          className="pic-one"
+          className="navbar__img"
           src={require('../../images/profile_pic1.jpg')}
           alt="Juan Sanchez's face"
         />
       </div>
-      <div className="second-half">
-        <ul className="container-anchor">
-          <li>
-            <a href="#jump-home" className="anchor-home">
+      <div className="navbar__list">
+        <ul className="navbar__ul">
+          <li className="navbar__li">
+            <a href="#jump-home" className="navbar__anchor">
               Home
             </a>
           </li>
 
-          <li>
-            <a href="#jump-projects" className="anchor-projects">
+          <li className="navbar__li">
+            <a href="#jump-projects" className="navbar__anchor">
               Projects
             </a>
           </li>
-          <li>
-            <a href="#jump-about" className="anchor-about">
+          <li className="navbar__li">
+            <a href="#jump-about" className="navbar__anchor">
               About
             </a>
           </li>
-          <li>
-            <a href="#jump-contact" className="anchor-contact">
+          <li className="navbar__li">
+            <a href="#jump-contact" className="navbar__anchor">
               Contact
             </a>
           </li>

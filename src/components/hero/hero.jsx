14616c6d8fa6b480639f3d9f5096f80ba2a2ebc7
Juan Sanchez <juansan1993@hotmail.com>
2023-02-28 21:55:50 -0500

    Add BEM naming convention

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,13 +3,14 @@ import './hero.css';
 
 export function Hero() {
   return (
-    <section className="hero-section">
-      <p className="hero-greeting elements-inside-section">Hello, my name is</p>
-      <h1 className="hero-name elements-inside-section">Juan Sanchez</h1>
-      <h2 className="hero-title elements-inside-section">
-        I create <span> good quality</span> projects for the web.
+    <section className="hero">
+      <p className="hero__greeting elements">Hello, my name is</p>
+      <h1 className="hero__name elements">Juan Sanchez</h1>
+      <h2 className="h2 hero__title elements">
+        I create <span className="h2__span"> good quality</span> projects for
+        the web.
       </h2>
-      <p className="hero-description elements-inside-section">
+      <p className="hero__description elements">
         As a front-end web developer with a strong interest in the backend, I am
         excited to present my portfolio website. Through this platform, I aim to
         demonstrate my skills and secure my first opportunity as a web

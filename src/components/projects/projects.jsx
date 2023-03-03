9e616aa7ff6a45a68a1261a0d63e79589ddaf549
Juan Sanchez <juansan1993@hotmail.com>
2023-03-02 22:30:57 -0500

    Update react icons, change to BEM notation (still working)

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,65 +1,65 @@
 import React from 'react';
 import './projects.css';
-
+import { GrGithub } from 'react-icons/gr';
+import { GrReactjs } from 'react-icons/gr';
+import { TiHtml5 } from 'react-icons/ti';
+import { SiJavascript } from 'react-icons/si';
 export function Projects() {
   return (
-    <div className="projects-header">
-      <div id="jump-projects" className="project-text">
-        <div className="project-text2">Projects</div>
-        <div className="project-line"></div>
+    <div className="projects">
+      <div className="project__header-container">
+        <div className="project__header">Projects</div>
+        <span className="project__line"></span>
       </div>
-      <section class="card-list">
+      <section class="cards">
         <article class="card">
-          <header class="card-header">
-            <h2>Rock paper scissors</h2>
+          <header class="card__header">
+            <h2 className="card__header-text">Rock paper scissors</h2>
           </header>
           <a target="_bla" href="https://luxury-swan-136d4f.netlify.app/">
             <img
-              className="img-cards"
+              className="img__cards"
               src={require('../../images/rock_paper_scissors_img.png')}
               alt="snake-game"
             />
           </a>
-          <div className="technologies">Technologies</div>
-          <div className="tags">
-            <div className="tech" href="#">
-              html
-            </div>
-            <div className="tech" href="#">
-              css
-            </div>
-            <div className="tech" href="#">
-              JS
-            </div>
-          </div>
+          <h2 className="technologies">Technologies</h2>
+          <ul className="tags">
+            <li className="tech">
+              <GrGithub />
+            </li>
+            <li className="tech">
+              <GrReactjs />
+            </li>
+            <li className="tech">
+              <TiHtml5 />
+            </li>
+            <li className="tech">
+              <SiJavascript />
+            </li>
+          </ul>
         </article>
 
         <article class="card">
-          <header class="card-header">
+          <header class="card__header">
             <h2>Snake</h2>
           </header>
           <a target="_bla" href="https://hilarious-medovik-3cda2f.netlify.app/">
             <img
-              className="img-cards"
+              className="img__cards"
               src={require('../../images/snake_game_img.png')}
               alt="snake-game"
             />
           </a>
-          <div className="technologies">Technologies</div>
-          <div className="tags">
-            <div className="tech" href="#">
-              html
-            </div>
-            <div className="tech" href="#">
-              css
-            </div>
-            <div className="tech" href="#">
-              JS
-            </div>
-          </div>
+          <h2 className="technologies">Technologies</h2>
+          <ul className="tags">
+            <li className="tech">html</li>
+            <li className="tech">css</li>
+            <li className="tech">JS</li>
+          </ul>
         </article>
         <article class="card">
-          <header class="card-header">
+          <header class="card__header">
             <h2>Clicks counter</h2>
           </header>
           <a
@@ -67,50 +67,35 @@ export function Projects() {
             href="https://incomparable-pothos-f8cec3.netlify.app/"
           >
             <img
-              className="img-cards"
+              className="img__cards"
               src={require('../../images/clicks_counter_img.png')}
               alt="snake-game"
             />
           </a>
-          <div className="technologies">Technologies</div>
-          <div className="tags">
-            <div className="tech" href="#">
-              React
-            </div>
-            <div className="tech" href="#">
-              Html
-            </div>
-            <div className="tech" href="#">
-              css
-            </div>
-            <div className="tech" href="#">
-              JS
-            </div>
-          </div>
+          <h2 className="technologies">Technologies</h2>
+          <ul className="tags">
+            <li className="tech">html</li>
+            <li className="tech">css</li>
+            <li className="tech">JS</li>
+          </ul>
         </article>
         <article class="card">
-          <header class="card-header">
+          <header class="card__header">
             <h2>Typing speed test</h2>
           </header>
           <a target="_bla" href="https://creative-entremet-225e0d.netlify.app/">
             <img
-              className="img-cards"
+              className="img__cards"
               src={require('../../images/typing_speed_test_img.png')}
               alt="snake-game"
             />
           </a>
-          <div className="technologies">Technologies</div>
-          <div className="tags">
-            <div className="tech" href="#">
-              html
-            </div>
-            <div className="tech" href="#">
-              css
-            </div>
-            <div className="tech" href="#">
-              JS
-            </div>
-          </div>
+          <h2 className="technologies">Technologies</h2>
+          <ul className="tags">
+            <li className="tech">html</li>
+            <li className="tech">css</li>
+            <li className="tech">JS</li>
+          </ul>
         </article>
       </section>
     </div>

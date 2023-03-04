04b45f9a87a47a2c67d952f98e8416e67f3d0b44
Juan Sanchez <juansan1993@hotmail.com>
2023-03-04 13:50:22 -0500

    Update skills with ul li

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -8,13 +8,43 @@ export function Skills() {
         <div className="skills__header">Skills</div>
         <span className="skills__line"></span>
       </header>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#5000ca"
-          fill-opacity="1"
-          d="M0,160L1440,224L1440,320L0,320Z"
-        ></path>
-      </svg>
+      <section className="skills__soft-hard">
+        <article className="skills__soft">
+          <h2>Soft skills</h2>
+          <ul className="skills_ul">
+            <li className="skills__li">Assertive communication</li>
+            <li className="skills__li">Time management</li>
+            <li className="skills__li">Problem-solving</li>
+            <li className="skills__li">Adaptability</li>
+            <li className="skills__li">Collaboration</li>
+          </ul>
+        </article>
+        <article className="skills__hard">
+          <h2>Hard skills</h2>
+          <ul className="skills__li">
+            <li className="skills__li">Assertive communication</li>
+            <li className="skills__li">Time management</li>
+            <li className="skills__li">Problem-solving</li>
+            <li className="skills__li">Adaptability</li>
+            <li className="skills__li">Collaboration</li>
+          </ul>
+        </article>
+      </section>
     </section>
   );
+}
+
+{
+  /* <svg
+  className="skills__svg"
+  fill="#da2d6d"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 500 550"
+>
+  <path
+    fill="#480aab"
+    fill-opacity="1"
+    d="M501.01,555.66 C449.66,524.85 478.99,209.04 510.60,620.90 L0.45,549.74 L499.88,-1.46 Z"
+  ></path>
+</svg> */
 }

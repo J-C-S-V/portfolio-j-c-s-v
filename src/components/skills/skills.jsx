1da0a32fef036ab3f0508561dcc53aed5fb0dcf9
Juan Sanchez <juansan1993@hotmail.com>
2023-03-04 12:32:17 -0500

    Delete skills

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,65 +1,20 @@
 import React from 'react';
 import './skills.css';
-import { FaReact } from 'react-icons/fa';
-import { SiJavascript } from 'react-icons/si';
-import { DiCss3 } from 'react-icons/di';
-import { FaGitAlt } from 'react-icons/fa';
-import { SiVisualstudio } from 'react-icons/si';
-import { TiHtml5 } from 'react-icons/ti';
 
 export function Skills() {
   return (
-    <div className="skills-main-container">
-      <div className="skills-heading">
-        <div className="skills-text">Skills</div>
-        <div className="skills-line"></div>
-      </div>
-      <div className="skills-container">
-        <ul className="skills-list">
-          <li>
-            <a rel="noreferrer" target="_blank" href="https://www.w3.org/html/">
-              <TiHtml5 />
-            </a>
-          </li>
-          <li>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            >
-              <SiJavascript />
-            </a>
-          </li>
-          <li>
-            <a rel="noreferrer" target="_blank" href="https://reactjs.org/">
-              <FaReact />
-            </a>
-          </li>
-          <li>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://www.w3.org/Style/CSS/"
-            >
-              <DiCss3 />
-            </a>
-          </li>
-          <li>
-            <a rel="noreferrer" target="_blank" href="https://git-scm.com/">
-              <FaGitAlt />
-            </a>
-          </li>
-          <li>
-            <a
-              rel="noreferrer"
-              target="_blank"
-              href="https://code.visualstudio.com/"
-            >
-              <SiVisualstudio />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <section className="skills">
+      <header className="skills__header-container">
+        <div className="skills__header">Skills</div>
+        <span className="skills__line"></span>
+      </header>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#5000ca"
+          fill-opacity="1"
+          d="M0,160L1440,224L1440,320L0,320Z"
+        ></path>
+      </svg>
+    </section>
   );
 }

61f34ebb0446615bb387a7d3f033b75ef23b3ca9
Juan Sanchez <juansan1993@hotmail.com>
2023-03-04 15:45:40 -0500

    Update left bar

--- src/components/leftbar/leftbar.jsx
+++ src/components/leftbar/leftbar.jsx
@@ -8,16 +8,30 @@ export function LeftBar() {
   return (
     <div className="left-bar">
       <a
+        className="left-bar__anchor"
+        title="LinkedIn"
         rel="noreferrer"
         target="_blank"
         href="https://www.linkedin.com/in/juan-carlos-sanchez-vargas-a308b014b/"
       >
         <AiFillLinkedin />
       </a>
-      <a rel="noreferrer" target="_blank" href="https://github.com/J-C-S-V">
+      <a
+        className="left-bar__anchor"
+        title="GitHub"
+        rel="noreferrer"
+        target="_blank"
+        href="https://github.com/J-C-S-V"
+      >
         <AiFillGithub />
       </a>
-      <a rel="noreferrer" target="_blank" href="https://twitter.com/juansan0">
+      <a
+        className="left-bar__anchor"
+        title="Twitter"
+        rel="noreferrer"
+        target="_blank"
+        href="https://twitter.com/juansan0"
+      >
         <AiOutlineTwitter />
       </a>
     </div>

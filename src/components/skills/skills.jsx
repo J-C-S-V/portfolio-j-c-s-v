4c961ba687232cca3f2323c9c8311df0f43e5343
Juan Sanchez <juansan1993@hotmail.com>
2023-03-04 15:29:17 -0500

    Update skills content

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -11,6 +11,7 @@ export function Skills() {
       <section className="skills__soft-hard">
         <article className="skills__soft">
           <h2>Soft skills</h2>
+          <br />
           <ul className="skills_ul">
             <li className="skills__li">Assertive communication</li>
             <li className="skills__li">Time management</li>
@@ -21,12 +22,13 @@ export function Skills() {
         </article>
         <article className="skills__hard">
           <h2>Hard skills</h2>
+          <br />
           <ul className="skills__li">
-            <li className="skills__li">Assertive communication</li>
-            <li className="skills__li">Time management</li>
-            <li className="skills__li">Problem-solving</li>
-            <li className="skills__li">Adaptability</li>
-            <li className="skills__li">Collaboration</li>
+            <li className="skills__li">Reactjs</li>
+            <li className="skills__li">JavaScript ES6</li>
+            <li className="skills__li">CSS3</li>
+            <li className="skills__li">SASS</li>
+            <li className="skills__li">HTML5</li>
           </ul>
         </article>
       </section>

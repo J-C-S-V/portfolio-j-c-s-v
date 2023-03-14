2c12181662e1e86c5134aa6cd92ab553a3b2a038
Juan Sanchez <juansan1993@hotmail.com>
2023-03-13 19:19:05 -0500

    Update jsx file names to Pascal. Add styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { Navbar } from './components/navbar/navbar.jsx';
-import { Hero } from './components/hero/hero.jsx';
-import { LeftBar } from './components/leftbar/leftbar.jsx';
-import { Projects } from './components/projects/projects.jsx';
-import { Footer } from './components/footer/footer.jsx';
-import { About } from './components/about/about.jsx';
-import { Skills } from './components/skills/skills.jsx';
+import { Navbar } from './components/navbar/Navbar';
+import { Hero } from './components/hero/Hero';
+import { LeftBar } from './components/leftbar/Leftbar';
+import { Projects } from './components/projects/Projects';
+import { Footer } from './components/footer/Footer';
+import { About } from './components/about/About';
+import { Skills } from './components/skills/Skills';
+import { Modal } from './components/portals/Modal.jsx';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <Footer />
       <LeftBar />
+      <Modal />
     </div>
   );
 }
---
+++ src/components/portals/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const portalRoot = document.getElementById('portal');
+
+export function Modal() {
+  return (
+    <>
+      <Overlay></Overlay>
+    </>
+  );
+}

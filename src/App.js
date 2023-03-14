ea720fb988283f25872906353de995efaead6872
Juan Sanchez <juansan1993@hotmail.com>
2023-03-13 22:22:53 -0500

    Update modal style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { useState } from 'react';
 import { Navbar } from './components/navbar/Navbar';
 import { Hero } from './components/hero/Hero';
 import { LeftBar } from './components/leftbar/Leftbar';
@@ -7,8 +7,12 @@ import { Footer } from './components/footer/Footer';
 import { About } from './components/about/About';
 import { Skills } from './components/skills/Skills';
 import { Modal } from './components/portals/Modal.jsx';
+import styled from 'styled-components';
+import './App.css';
 
 function App() {
+  const [modal1, setModal] = useState(true);
+
   return (
     <div className="App" id="jump-home">
       <Navbar />
@@ -19,8 +23,52 @@ function App() {
       <Footer />
       <LeftBar />
       <Modal />
+      {/* <Modal estado={modal1} cambiarEstado={setModal}>
+        <Contenido>
+          <h1>Ventana modal</h1>
+          <p>Reutilizable y con opciones de personalización</p>
+          <Boton onClick={() => setModal(!modal1)}>Dale!</Boton>
+        </Contenido>
+      </Modal> */}
     </div>
   );
 }
 
 export default App;
+
+const Contenido = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #000;
+  text-align: center;
+
+  h1 {
+    font-size: 42px;
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+
+  img {
+    width: 100%;
+    vertical-align: top;
+    border-radius: 3px;
+  }
+`;
+
+const Boton = styled.button`
+  padding: 10px 20px;
+  background: #000;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: all 0.3s ease;
+`;
--- src/components/portals/Modal.jsx
+++ src/components/portals/Modal.jsx
@@ -1,12 +1,92 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const portalRoot = document.getElementById('portal');
-
-export function Modal() {
+export function Modal({ children, estado, cambiarEstado }) {
   return (
     <>
-      <Overlay></Overlay>
+      {estado && (
+        <Overlay>
+          <ContenedorModal>
+            <EncabezadoModal>
+              <h3>Title</h3>
+            </EncabezadoModal>
+            <BotonCerrar onClick={() => cambiarEstado(false)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-x"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+            </BotonCerrar>
+            {children}
+          </ContenedorModal>
+        </Overlay>
+      )}
     </>
   );
 }
+
+const Overlay = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+`;
+
+const ContenedorModal = styled.div`
+  width: 500px;
+  min-height: 100px;
+  background-color: #fff;
+  position: relative;
+  border-radius: 10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  padding: 20px;
+`;
+
+const EncabezadoModal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+
+  h3 {
+    font-weight: 500;
+    font-size: 16px;
+    color: #1766dc;
+  }
+`;
+
+const BotonCerrar = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 30px;
+  height: 30px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  border-radius: 5px;
+  color: #1766dc;
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;

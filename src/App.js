2d4ee9ff9888f2343e58a60fc138c71f34cf05c3
Juan Sanchez <juansan1993@hotmail.com>
2023-03-14 19:04:13 -0500

    Update navbar name3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Navbar } from './components/navbar/Navbar.jsx';
+import { Navbar } from './components/navbar/Navbar';
 import { Hero } from './components/hero/Hero.jsx';
 import { LeftBar } from './components/leftbar/Leftbar.jsx';
 import { Projects } from './components/projects/Projects.jsx';

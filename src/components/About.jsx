428c8c7b21c75875b872c5765a10110af6248ba5
Juan Sanchez <juansan1993@hotmail.com>
2023-06-03 18:44:16 -0500

    Fix overflow coming from the right

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,13 +16,13 @@ export default function About() {
       </header>
       <div className="about__container" data-aos="fade-up">
         <div className="about__p-container">
-          <p className="about__p about__p-1" data-aos="fade-left">
+          <p className="about__p about__p-1" data-aos="fade-right">
             I&apos;m a music teacher who developed an interest in coding after
             seeing the positive impact it can have on others. I grew up around
             computers and had to choose between studying music or systems
             engineering, ultimately choosing music. 🎼
           </p>
-          <p className="about__p about__p-img" data-aos="fade-left">
+          <p className="about__p about__p-img" data-aos="fade-right">
             I completed a 5-year music program and taught in schools for 3
             years, but now I want to change careers to become a successful web
             developer. 💻
@@ -31,7 +31,7 @@ export default function About() {
             My goal right now is to gain experience and knowledge to help other
             people and make this world a better place to live for everyone. 🌎
             <img
-              data-aos="fade-left"
+              data-aos="fade-right"
               className="about__img"
               src={profilePicTwo}
               alt="Juan Sanchez"

b4d8d5bf6ef9b3c0c1497f8b64bee468141dc9e8
Juan Sanchez <juansan1993@hotmail.com>
2023-06-03 22:55:29 -0500

    Update technologies style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,15 +12,14 @@ export default function Hero() {
       <p className="hero__greeting elements">Hello, my name is</p>
       <h1 className="hero__name elements">Juan Sanchez</h1>
       <h2 className="h2 hero__title elements">
-        I create
-        <span className="h2__span"> good quality</span>
-        projects for the web.
+        <span className="h2__span">Excellence </span>
+        as the cornerstone.
       </h2>
       <p className="hero__description elements">
-        As a front-end web developer With a strong interest in both front-end
-        and backend, I am thrilled to showcase my portfolio website through this
-        platform and to demonstrate my skills that I&apos;ve aquired with the
-        folowing projects. 😃✨💻
+        As a front-end web developer, I am thrilled to showcase my portfolio website through this
+        platform and to demonstrate my skills that I&apos;ve acquired with the
+        folowing projects. If you like what you see and
+        have a project you need coded, don&apos;t hestiate to contact me. 😃✨💻
       </p>
     </section>
   );
